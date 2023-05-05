3bd36692f55e8e3017f285f411cb8d1f081a6191
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-04 23:46:13 -0500

    added event listeners for screens to display and disappear.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
-var takeQuizBtn = document.querySelector(".take-quiz-btn");
-var timer = document.querySelector(".timer");
-console.log(timer);
+var start = document.querySelector("#start");
+var quiz = document.querySelector("#quiz");
+var end = document.querySelector("#end");
+var startQuiz = document.querySelector(".take-quiz-btn");
+var endQuiz = document.querySelector(".end-quiz-btn");
+var quizHome = document.querySelector(".home-quiz-btn");
 
-takeQuizBtn.addEventListener("click", function takeQuiz() {
-  var timeLeft = 15;
-
-  var myInterval = setInterval(function () {
-    timeLeft--;
-    timer.textContent = timeLeft;
-
-    if (timeLeft === 0) {
-      clearInterval(myInterval);
-      timer.textContent = 0;
-    }
-  }, 1000);
+startQuiz.addEventListener("click", function () {
+  start.setAttribute("style", "display:none;");
+  quiz.setAttribute("style", "display:;");
+  end.setAttribute("style", "display:none;");
 });
+endQuiz.addEventListener("click", function () {
+  start.setAttribute("style", "display:none;");
+  quiz.setAttribute("style", "display:none;");
+  end.setAttribute("style", "display:;");
+});
+
+quizHome.addEventListener("click", function () {
+  start.setAttribute("style", "display:;");
+  quiz.setAttribute("style", "display:none;");
+  end.setAttribute("style", "display:none;");
+});
+
+// var takeQuizBtn = document.querySelector(".take-quiz-btn");
+// var timer = document.querySelector(".timer");
+// console.log(timer);
+
+// takeQuizBtn.addEventListener("click", function takeQuiz() {
+//   var timeLeft = 15;
+
+//   var myInterval = setInterval(function () {
+//     timeLeft--;
+//     timer.textContent = timeLeft;
+
+//     if (timeLeft === 0) {
+//       clearInterval(myInterval);
+//       timer.textContent = 0;
+//     }
+//   }, 1000);
+// });
