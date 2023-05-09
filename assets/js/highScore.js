e1de04f8b5243381aac08b87bfbacf751df6847c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-09 14:14:12 -0500

    added a function and the sort method to sort addScore array from highest to lowest before being displayed iterated over in for loop.

--- assets/js/highScore.js
+++ assets/js/highScore.js
@@ -16,6 +16,10 @@ function renderPlayersScores() {
   // only added the if statement so no error would show if the highScoreData array hasn't been created in local storage yet
   if (localStorage.getItem("highScoreData")) {
     var addScore = JSON.parse(localStorage.getItem("highScoreData"));
+    // function to grab addScore array and sort by score from highest to lowest.
+    addScore.sort(function (a, b) {
+      return b.newScore - a.newScore;
+    });
     console.log(addScore);
     for (let index = 0; index < addScore.length; index++) {
       var scoreLi = document.createElement("li");
